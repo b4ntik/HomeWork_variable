5feb3313173285d027e0b089dd9b435a87c1f70c
bntik <bannikov.ilya2017@yandex.ru>
2025-02-20 23:38:07 +0500

    some homeworks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
+        byte byteNumber = 127;
+        short shortNumber = 32767;
+        int intNumber = 2147483647;
+long longNumber = -9223372036854775808L;
+float floatNumber = 3.1415926f;
+double doubleNumber = 9.999999999999999;
+        System.out.println("Значение переменной byteNumber с типом byte равно " + byteNumber);
+        System.out.println("Значение переменной shortNumber с типом short равно "+ shortNumber);
+        System.out.println("Значение переменной intNumber с типом int равно " + intNumber);
+        System.out.println("Значение переменной longNumber с типом long равно " + longNumber);
+        System.out.println("Значение переменной floatNumber с типом float равно "+ floatNumber);
+        System.out.println("Значение переменной doubleNumber с типом double равно "+ doubleNumber);
+float number = 27.12f;
+long elseLong = 987678965549L;
+float numberTwo = 2.786f;
+short numberThree = 569;
+short numberFour = -159;
+short numberFive = 27897;
+byte numberSix = 67;
+short classLP = 23;
+short classAS = 27;
+short classEA = 30;
+int everyStudentPaper = 480 / (classEA+classAS+classLP);
+System.out.println("На каждого ученика рассчитано " + everyStudentPaper + " листов бумаги");
+
+byte performanceBottle = 16/2;
+
+System.out.println("За 20 минут машина произвела " + performanceBottle*20 + " бутылок");
+        System.out.println("За сутки машина произвела " + performanceBottle*60*24 + " бутылок");
+        System.out.println("За три дня машина произвела " + performanceBottle*60*24*3 + " бутылок");
+        System.out.println("За месяц машина произвела " + performanceBottle*60*24*30 + " бутылок");
+
 
     }
 }
